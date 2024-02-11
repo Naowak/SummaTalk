b9e5248d21ebb04b5be8171048d7ed51cc6a0ea4
Naowak <yannis.bendiouis@gmail.com>
2024-02-11 11:27:50 +0100

    make summaries

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
+import requests
 from flask import Flask, render_template, redirect, url_for, request
+import concurrent.futures
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
+
+# Prompt
+PROMPT = """Je vais t'envoyer un texte commencant et finissant par des guillemets.
+- Tu es un secretaire.  
+- On veut un compte rendu en markdown de ce texte tire de l'enregistrement d'une video. 
+
+Instruction importantes:
+- Ecrit entierement ta reponse en markdown.
+- Tu dois ecrire en markdown.
+
+Tes instructions :
+- Structure l'information.
+- Donne un titre au texte.
+- Decoupe le texte en 2 ou 3 grandes parties en les numerotant et titre les.
+- Decoupe les parties en sous partie en les numerorant.
+- Fait un point pour chaque information.
+- Fait des phrases avec un sujet, un verbe et un complement et un determinant pour les noms pour chaque point.
+- Ecrit en francais.
+- Si tu veux ecrire le texte en anglais, ecrit en francais.
+- On veut que l'information soit structure.
+- Ecrit entierement ta reponse en markdown, c'est important.
+- Tu dois ecrire en markdown. 
+- Ne repete pas les memes informations.
+- Ne fait pas de conclusion.  
+"""
 
 # Load the model
 model = SentenceTransformer('flaubert/flaubert_base_uncased')
@@ -18,8 +45,41 @@ def find_closest_chunk(query, df, model, n=3):
 
 def get_file_chunk(filename, df):
     chunks = df[df.filename == filename.split('.')[0]].text.tolist()
-    print("chunkkk", chunks)
-    return ''.join(chunks)
+    return chunks
+
+def group_chunks_by(chunks, nb):
+    return [chunks[i:i+nb] for i in range(0, len(chunks), nb)]
+
+def make_completion_request(prompt):
+    url = 'http://localhost:8000/v1/completions'
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "model": "mistralai/Mistral-7B-Instruct-v0.2",
+        "prompt": prompt,
+        "max_tokens": 4096,
+        "temperature": 0.7
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()
+
+def make_several_completion_requests(prompts):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(make_completion_request, prompts)
+    results = list(results)
+    return results
+
+def make_summary(filename):
+    # Retrieve chunks
+    chunks = group_chunks_by(get_file_chunk(filename, df), 8)
+    prompts = []
+    for chunk in chunks:
+        prompt = PROMPT + "\"" + ''.join(chunk) + "\"\n\n"
+        prompts.append(prompt)
+    results = make_several_completion_requests(prompts)
+    summaries = [result['choices'][0]['text'] for result in results]
+
+    result = make_completion_request(PROMPT + "\"" + ''.join(summaries) + "\"\n\n")
+    return result['choices'][0]['text'] 
 
 # Load the App
 app = Flask(__name__)
@@ -61,6 +121,10 @@ def upload_file():
         file.filename
         return redirect(url_for('result', filename=file.filename))
     return redirect(url_for('upload'))
+
+@app.route('/summary/<filename>')
+def summary(filename):
+    return make_summary(filename)
 
 
 
